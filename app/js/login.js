6aea23aa9edc86e5678abc54a5206c235909777e
Sofie <sofie@tii.se>
2016-04-16 19:12:29 +0200

    firebase

--- app/js/login.js
+++ app/js/login.js
@@ -2,7 +2,7 @@
 
 //Fetched from https://www.firebase.com/docs/web/guide/login/password.html
 
-var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
+var ref = new Firebase("https://sweltering-inferno-7067.firebaseio.com");
 ref.createUser({
   email    : "bobtony@firebase.com",
   password : "correcthorsebatterystaple"
@@ -17,7 +17,7 @@ ref.createUser({
 
 //logging in
 
-var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
+var ref = new Firebase("https://sweltering-inferno-7067.firebaseio.com");
 ref.authWithPassword({
   email    : "bobtony@firebase.com",
   password : "correcthorsebatterystaple"
