9dcd869b5739ee9b55ece85f4ba83996abd76e7f
ludwan <ludwigsidenmark@gmail.com>
2016-04-17 17:35:22 +0200

    Registering now users now works (Email + password)

--- app/js/loginCtrl.js
+++ app/js/loginCtrl.js
@@ -1,3 +1,11 @@
 musiksalenApp.controller('LoginCtrl', function($scope){
 
+	$scope.login = function(){
+		console.log("We are loging in");
+		console.log($scope.user);
+		console.log($scope.password);
+
+		$scope.loading = true;
+	}
+
 });
