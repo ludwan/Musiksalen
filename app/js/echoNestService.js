45305a8d64ac0da4ea800b9eca7760848cf107fe
ludwan <ludwigsidenmark@gmail.com>
2016-07-09 00:06:51 +0200

    Fix

--- app/js/echoNestService.js
+++ app/js/echoNestService.js
@@ -2,7 +2,7 @@ musiksalenApp.service('echoNestService', function ($resource){
     
     var apiKey = '4IGS4L1R7UE1N2UKS';
     //Searches for artists
-    this.ArtistSearch = $resource('https://developer.echonest.com/api/v4/artist/search?bucket=artist_location', {
+    this.ArtistSearch = $resource('http://developer.echonest.com/api/v4/artist/search?bucket=artist_location', {
         format : 'json',
         api_key : apiKey,
         sort : 'familiarity-desc',
@@ -10,25 +10,25 @@ musiksalenApp.service('echoNestService', function ($resource){
     });
     
     //Returns information about artists
-    this.getArtist = $resource('https://developer.echonest.com/api/v4/artist/profile?bucket=artist_location&bucket=years_active&bucket=genre',{
+    this.getArtist = $resource('http://developer.echonest.com/api/v4/artist/profile?bucket=artist_location&bucket=years_active&bucket=genre',{
         format : 'json', 
         api_key : apiKey
     });
 
     //Returns only the most basic information about artists (name and id)
-    this.getBasicArtist = $resource('https://developer.echonest.com/api/v4/artist/profile?', {
+    this.getBasicArtist = $resource('http://developer.echonest.com/api/v4/artist/profile?', {
         format : 'json',
         api_key : apiKey
     });
 
     //Returns information about work (name and id of both song and artist)
-    this.getWork = $resource('https://developer.echonest.com/api/v4/song/profile?',{
+    this.getWork = $resource('http://developer.echonest.com/api/v4/song/profile?',{
         format : 'json',
         api_key : apiKey
     });
 
     //Returns songs related to an artist. Does not return duplicates
-    this.workPlaylistSearch = $resource('https://developer.echonest.com/api/v4/playlist/static?&bucket=song_hotttnesss&bucket=song_currency', {
+    this.workPlaylistSearch = $resource('http://developer.echonest.com/api/v4/playlist/static?&bucket=song_hotttnesss&bucket=song_currency', {
         format : 'json',
         type : 'artist',
         api_key : apiKey,
